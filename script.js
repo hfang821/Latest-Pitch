55a4f3077d6badaf4f97d8f9ca03611d156e688f
hfang821 <95199209+hfang821@users.noreply.github.com>
2022-04-09 18:36:56 -0400

    using Moment.js to replace the second world clock API for better UI

--- script.js
+++ script.js
@@ -2,6 +2,33 @@ var englandTimeEl = document.getElementById("englandTime");
 var ghanaTimeEl = document.getElementById("GhanaTime"); 
 var localTimeEl = document.getElementById("localTime");
 
+var updateEngland = function() {
+    document.getElementById("datetime")
+    englandTimeEl.innerHTML = "England Time: " + moment.utc().add(1, "hours").format('MMMM Do YYYY, h:mm:ss a')
+}
+setInterval(updateEngland, 1000);
+
+
+var updateGhana = function() {
+    document.getElementById("datetime")
+    ghanaTimeEl.innerHTML = "Ghana Time: " + moment.utc().format('MMMM Do YYYY, h:mm:ss a')
+}
+setInterval(updateGhana, 1000)
+
+
+var updateLocal = function() {
+        localTimeEl.innerHTML = "Local Time: " + moment().format('MMMM Do YYYY, HH:mm:ss a');       
+}
+setInterval(updateLocal,1000);
+
+
+/*
+Note: We have made several approaches to the SECOND API of World clock (As seen in the below codes)
+(even though it does the job of getting the JSON object with the correct time, there is no way to use the setInterval function to make it tick)
+Therefore: We decided to not use it and instead implement the Moment.js method to make our clock looks more interactive.
+*/
+
+/*
 var worldClockSearch = function(area, location) {
     
      var apiUrl3 = 'http://worldtimeapi.org/api/timezone/' + area +'/' + location;
@@ -26,6 +53,11 @@ var worldClockSearchGhana = function(area, location) {
    .catch(err => console.log(err));
 }
 
+var displayGhanaTime = function (timeData) {
+    var time = timeData.datetime;
+    //time = moment(time).format('MMMM Do YYYY, h:mm:ss a');
+   // ghanaTimeEl.innerHTML = 'Ghana Time: ' + time ;
+}
 
 var displayTime = function(timeData) {
     var time = timeData.datetime
@@ -42,30 +74,7 @@ var displayTime = function(timeData) {
 // console.log("london",london)
  //setInterval(london, 1000)
 
- var updateEngland = function() {
-    document.getElementById("datetime")
-    englandTimeEl.innerHTML = "England Time: " + moment.utc().add(1, "hours").format('MMMM Do YYYY, h:mm:ss a')
-}
-setInterval(updateEngland, 1000);
-
-
-var updateGhana = function() {
-    document.getElementById("datetime")
-    ghanaTimeEl.innerHTML = "Ghana Time: " + moment.utc().format('MMMM Do YYYY, h:mm:ss a')
-}
-setInterval(updateGhana, 1000)
-
-var displayGhanaTime = function (timeData) {
-    var time = timeData.datetime;
-    //time = moment(time).format('MMMM Do YYYY, h:mm:ss a');
-   // ghanaTimeEl.innerHTML = 'Ghana Time: ' + time ;
-}
-
-
 window.addEventListener("load", worldClockSearch('Europe','London'));
 window.addEventListener("load", worldClockSearchGhana('Africa','Accra'));
-window.addEventListener("load", function(){
-    var time = moment().format('MMMM Do YYYY, HH:mm:ss');
-    localTimeEl.append(time);
-
-})
+window.addEventListener("load", console.log(target));
+*/
